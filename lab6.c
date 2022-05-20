f703c2febac443b40d9612c8c34ac69474983924
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-05-20 15:35:23 -0300

    por algun lado se empieza

---
+++ lab6.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <ulfius.h>
+#include <string.h>
+#include <jansson.h>
+#include <regex.h>
+
+#define PORT 8080
+
+int valor=0;
+
+int callback_post_users (const struct _u_request * request, struct _u_response * response, void * user_data) {
+    
+    regex_t reegex;
+    int value;
+    json_t * json_request = ulfius_get_json_body_request(request,NULL);
+    json_t * username = json_object_get(json_request,"username");
+    json_t * password = json_object_get(json_request,"password");
+    char * user = json_string_value(username);
+    char * pass = json_string_value(password);
+    value = regcomp(&reegex,"^[a-z]+$",0);
+    if(value != 0) printf("RegEx error.");
+    value = regexec(&reegex,user,0,NULL,0);
+    if(value != 0){
+        json_t * string = json_string("user no tiene solo minusculas.");
+        json_t * body = json_object();
+        value = json_object_set(body,"mensaje",string);
+        if(value != 0) printf("json set object error.");
+        ulfius_set_json_body_response(response,200,body);
+        return U_CALLBACK_CONTINUE;
+    } 
+    value = regexec(&reegex,pass,0,NULL,0);
+    if(value != 0){
+        printf("pass no tiene solo minusculas.");
+    }
+    
+
+
+    
+}
+
+int callback_get_users (const struct _u_request * request, struct _u_response * response, void * user_data) {
+    ulfius_set_string_body_response(response, 200, "esto es get");
+    return U_CALLBACK_CONTINUE;
+}
+
+/**
+ * main function
+ */
+int main(void) {
+  struct _u_instance instance;
+
+  // Initialize instance with the port number
+  if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK) {
+    fprintf(stderr, "Error ulfius_init_instance, abort\n");
+    return(1);
+  }
+
+  // Endpoint list declaration
+  ulfius_add_endpoint_by_val(&instance, "POST", "/api/users", NULL, 0, &callback_post_users, NULL);
+  ulfius_add_endpoint_by_val(&instance, "GET", "/api/users", NULL, 0, &callback_get_users, NULL);
+
+  // Start the framework
+  if (ulfius_start_framework(&instance) == U_OK) {
+    printf("Start framework on port %d\n", instance.port);
+
+    // Wait for the user to press <enter> on the console to quit the application
+    getchar();
+  } else {
+    fprintf(stderr, "Error starting framework\n");
+  }
+  printf("End framework\n");
+
+  ulfius_stop_framework(&instance);
+  ulfius_clean_instance(&instance);
+
+  return 0;
+}
