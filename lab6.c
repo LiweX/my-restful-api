5f7026473dcfe10ec993ce811e853a917f1eab55
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-05-20 23:06:23 -0300

    servicios conectandose entre si con exito

--- lab6.c
+++ lab6.c
@@ -5,19 +5,23 @@
 
 #define PORT 8082
 
+json_t * data;
+
+
 int callback_post_users (const struct _u_request * request, struct _u_response * response, void * user_data) {
     (void)user_data;
     regex_t reegex;
     int value;
+    //parseo de request
     json_t * json_request = ulfius_get_json_body_request(request,NULL);
     json_t * username = json_object_get(json_request,"username");
     json_t * password = json_object_get(json_request,"password");
     const char * user = json_string_value(username);
     const char * pass = json_string_value(password);
-
+    //creacion de regex para solo aceptar minuculas
     value = regcomp(&reegex,"[^a-z]",0);
     if(value != 0) printf("RegEx error.");
-
+    //comprobacion de usuario
     value = regexec(&reegex,user,0,NULL,0);
     if(value == 0){
         json_t * string = json_string("user no tiene solo minusculas.");
@@ -27,7 +31,7 @@ int callback_post_users (const struct _u_request * request, struct _u_response *
         ulfius_set_json_body_response(response,200,body);
         return U_CALLBACK_CONTINUE;
     } 
-
+    //comprobacion de password
     value = regexec(&reegex,pass,0,NULL,0);
     if(value == 0){
         json_t * string = json_string("pass no tiene solo minusculas.");
@@ -37,7 +41,7 @@ int callback_post_users (const struct _u_request * request, struct _u_response *
         ulfius_set_json_body_response(response,200,body);
         return U_CALLBACK_CONTINUE;
     }
-    
+    //creacion de request para hacer get al contador de usuarios
     struct _u_request * aux_request = malloc(sizeof(struct _u_request));
     value = ulfius_init_request(aux_request);
     if(value!=0) printf("json error");
@@ -52,11 +56,11 @@ int callback_post_users (const struct _u_request * request, struct _u_response *
     if(value!=0) printf("json error");
     value = ulfius_send_http_request(aux_request,aux_response);
     if(value!=0) printf("json error");
-    
+    //parseo del response obtenido por el request anterior
     json_t * json_aux_request = ulfius_get_json_body_response(aux_response,NULL);
     json_t * json_aux_object = json_object_get(json_aux_request,"description");
     long id = json_integer_value(json_aux_object);
-
+    //preparar el json que sera el response
     json_t * body = json_object();
     time_t rawtime;
     struct tm * timeinfo;
@@ -71,14 +75,30 @@ int callback_post_users (const struct _u_request * request, struct _u_response *
     value = json_object_set(body,"created_at",json_string(logtime));
     if(value!=0) printf("json object error");
     ulfius_set_json_body_response(response,200,body);
+
+
+    json_t * data_object = json_object();
+    value = json_object_set_new(data_object,"user_id",json_integer(id));
+    if(value!=0) printf("json object error");
+    value = json_object_set_new(data_object,"username",json_string(user));
+    if(value!=0) printf("json object error");
+    value = json_array_append_new(data,data_object);
+    if(value!=0) printf("json array error");
+
     return U_CALLBACK_CONTINUE;
     
 }
 
-// int callback_get_users (const struct _u_request * request, struct _u_response * response, void * user_data) {
-//     ulfius_set_string_body_response(response, 200, "esto es get");
-//     return U_CALLBACK_CONTINUE;
-// }
+int callback_get_users (const struct _u_request * request, struct _u_response * response, void * user_data) {
+    request=request;
+    (void)user_data;
+    int value;
+    json_t * json_response = json_object();
+    value=json_object_set_new(json_response,"data",data);
+    if(value!=0) printf("json data error");
+    ulfius_set_json_body_response(response,200,json_response);
+    return U_CALLBACK_CONTINUE;
+}
 
 /**
  * main function
@@ -94,8 +114,8 @@ int main(void) {
 
   // Endpoint list declaration
   ulfius_add_endpoint_by_val(&instance, "POST", "/api/users", NULL, 0, &callback_post_users, NULL);
-//   ulfius_add_endpoint_by_val(&instance, "GET", "/api/users", NULL, 0, &callback_get_users, NULL);
-
+  ulfius_add_endpoint_by_val(&instance, "GET", "/api/users", NULL, 0, &callback_get_users, NULL);
+  data = json_array();
   // Start the framework
   if (ulfius_start_framework(&instance) == U_OK) {
     printf("Start framework on port %d\n", instance.port);
